a6fbc790994f745f5038b306c09eca48a9c0353b
mayabay <mayabay@web.de>
2021-03-06 17:57:19 +0100

    20210306Ftb

---
+++ assesment/Q7.java
@@ -0,0 +1,15 @@
+package assesment;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+
+public class Q7 {
+
+	public static void main(String[] args) {
+		Path path1 = Paths.get("/bats/night","../");
+		System.out.println(path1);
+	}
+
+}
