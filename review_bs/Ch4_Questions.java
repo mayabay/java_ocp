3262981b993611b0a32f1a30be56ea0038ee1cf0
mayabay <mayabay@web.de>
2021-03-09 20:11:15 +0100

    20210309amy

--- review_bs/Ch4_Questions.java
+++ review_bs/Ch4_Questions.java
@@ -4,7 +4,11 @@
 package review_bs;
 
 import java.util.function.Predicate;
+import java.util.function.Function;
 import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.IntStream;
 
 /**
  * 
@@ -18,7 +22,11 @@ public class Ch4_Questions {
 	public static void main(String[] args) {
 		Ch4_Questions o = new Ch4_Questions();
 		//o.q2();
-		o.q3();
+		//o.q3();
+		//o.q6();
+		//o.q11();
+		o.q14();
+
 
 	}
 
@@ -43,4 +51,32 @@ public class Ch4_Questions {
 		boolean b2 = stream2.peek(System.out::println).anyMatch(pred);
 		System.out.println(b1 + " " + b2 );
 	}
+
+	private void q6(){
+		//Stream<String> s = Stream.generate( ()-> "meow" );
+		Stream<String> s = Stream.of( "", "" );
+		boolean match = s
+		.peek((sstr)-> System.out.print("element down .. |") )
+		.peek(System.out::print)
+		.peek((str)-> System.out.print("| .. ") )
+		.allMatch( String::isEmpty );
+		System.out.println(match);
+	}
+
+	private void q11(){
+		String s =
+		Stream.iterate( 1, x -> x++ )
+		.limit(5)
+		.map(x-> "" + x )
+		.collect(Collectors.joining() );
+
+		System.out.println( s );
+	}
+
+	private void q14(){
+		DoubleStream ds = DoubleStream.of( 1.1,1.2,1.3 );
+		IntStream is = ds.mapToInt( x -> (int)x );
+		Stream<Integer> is2 = ds.mapToInt( x -> (int)x ).mapToObj( x->x ) ;
+	}
+
 }
